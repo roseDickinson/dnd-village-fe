551ee99aabd2ce5d4b0503d3382d1d32e701724d
Rose Dickinson <rose.dickinson@stfc.ac.uk>
2021-08-19 21:35:26 +0100

    feat: Installing extra dependencies and minor customisations

--- src/App.vue
+++ src/App.vue
@@ -10,49 +10,33 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="./assets/logo.png"
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
       </div>
+      <span>
+        DnD Location Visualisation
+      </span>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <LocationsTable/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import LocationsTable from './components/LocationsTable';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    LocationsTable,
   },
-
   data: () => ({
     //
   }),
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+              primary: "#58180d",
+              secondary: "#f2ece4",
+              accent: "#c9ad6a"
+            }
+        }
+    }
 });
